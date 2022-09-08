535a8537fb7e2b187b09a7b3121c7cf495f909f9
Marcelo Di Liscia <diliscia@gmail.com>
2022-09-08 17:39:42 -0400

    Update app.js
    update

--- src/main/java/app.js
+++ src/main/java/app.js
@@ -70,25 +70,31 @@ function searchPersonByName() {
     removeAll()
     let apiUrl = 'https://swapi.dev/api/people'
     axios.get(apiUrl).then(response =>{
-    const person = document.getElementById('person').value
-    if (person === "") {
-        li.innerText =  "Please enter a name\n"
-        list.appendChild(li);
-    }
+        const person = document.getElementById('person').value
 
-    for (i = 0; i <= response.data.results.length; i++) {
-        if (response.data.results[i].name === person) {
-            {
-            li.innerText =  "Person found\n\n" + "Basic Info:\n"
-            + "\nName: " + response.data.results[i].name 
-            + "\nDOB: " + response.data.results[i].birth_year 
-            + "\nHeight: " + response.data.results[i].height + " cm"
-            + "\nMass: " + response.data.results[i].mass + " kg"
+
+        for (i = 0; i <= response.data.results.length; i++) {
+            if (response.data.results[i].name === person) {
+                {
+                li.innerText =  "Person found\n\n" + "Basic Info:\n"
+                + "\nName: " + response.data.results[i].name 
+                + "\nDOB: " + response.data.results[i].birth_year 
+                + "\nHeight: " + response.data.results[i].height + " cm"
+                + "\nMass: " + response.data.results[i].mass + " kg"
+                }
+               
+            } 
+            if (response.data.results[i].name !== person){
+                if (person === "") {
+                    li.innerText =  "Please enter a name\n"
+                    list.appendChild(li);
+                } else {
+                li.innerText =  "Person NOT found\n\n";
+                }
             }
             list.appendChild(li);
-        }
-    }
-}) 
+        } 
+    }) 
 };
 
 buttonPeople.addEventListener('click', getInfoPeople)
